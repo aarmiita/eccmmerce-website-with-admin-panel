80f5abdc88d69714357d0bc46bc0d31251d182ab
aarmiita <aarmiita@yahoo.com>
2021-07-30 23:47:45 +0430

    i added payment handling for suceessfull paymenta

--- src/components/Payments/Payment.jsx
+++ src/components/Payments/Payment.jsx
@@ -1,16 +1,26 @@
 import { Button } from "@material-ui/core";
-import React from "react";
+import React, { useState } from "react";
 import payment from "../../assets/images/payment.png";
 import { useStyles } from "../../styles";
 const Payment = () => {
   const classes = useStyles();
+  const orders = JSON.stringify({ orders: [] });
+  const cartFromLocalStorage = JSON.parse(
+    localStorage.getItem("cart") || orders
+  );
+  const [cart, setCart] = useState({});
+  const handleSuccessPayment = () => {};
   return (
     <div className="payment">
       <div className="payment__pic__div">
         <img src={payment} alt="payment" className="payment__pic__div__img" />
       </div>
       <div className="payment__btn__div">
-        <Button variant="contained" className="payment__btn__div__successbtn">
+        <Button
+          variant="contained"
+          className="payment__btn__div__successbtn"
+          onClick={() => handleSuccessPayment()}
+        >
           <strong>پرداخت موفق</strong>
         </Button>
         <Button
