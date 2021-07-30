1688a7b731e169b66f947270812ecd4eb9943a12
aarmiita <aarmiita@yahoo.com>
2021-07-30 21:23:06 +0430

    i made payment page and add payment component in it

---
+++ src/pages/PaymentPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Payment from "./../components/Payments/Payment";
+const PaymentPage = () => {
+  return (
+    <>
+      <Payment />
+    </>
+  );
+};
+
+export default PaymentPage;
